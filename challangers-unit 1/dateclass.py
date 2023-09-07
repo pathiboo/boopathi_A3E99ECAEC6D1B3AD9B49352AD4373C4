021129f36f595c619b5c335224781193aef86208
pathiboo <144333133+pathiboo@users.noreply.github.com>
2023-09-07 15:37:07 +0530

    Add files via upload

---
+++ challangers-unit 1/dateclass.py
@@ -0,0 +1,7 @@
+import datetime
+today=datetime.date.today()
+print(today)
+print('ctime:',today.ctime())
+print('year:',today.year)
+print('month:',today.month)
+print('day:',today.day)
---
+++ challangers-unit 1/leepyear.py
@@ -0,0 +1,6 @@
+def leepyear(x):
+    if x%4==0:print("The given year",x,"is leep year")
+    else:print("The given year",x,"is not a leep year")
+year=int(input("enterr a year:"))
+leepyear(year)
+            
